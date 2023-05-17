665ccf1cf0c1fe0a0c2fe40912ea61d842a211e5
André Carvalho <andrecarvalhoprado@gmail.com>
2023-05-17 18:21:42 -0300

    Avanços "Adote já"

--- src/pages/Adote/index.js
+++ src/pages/Adote/index.js
@@ -1,10 +1,20 @@
+import Titulo from 'components/Titulo';
 import styles from './Adote.module.css';
 
 function Adote(){
     return(
-        <>
-            AAAAA
-        </>
+        <div className={styles.linha}>
+            <div className={styles.divSidebar}>
+                <Titulo>
+                    <h2 className={styles.titulo}>FILTRO_AQUI</h2>
+                </Titulo>
+            </div>
+            <div className={styles.divAnimais}>
+                <Titulo>
+                    <h2 className={styles.titulo}>Animais disponíveis</h2>
+                </Titulo>
+            </div>
+        </div>
     )
 }
 

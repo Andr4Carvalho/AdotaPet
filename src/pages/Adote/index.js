a114a7ca990977ccfa24916b663604c62e844580
André Carvalho <andrecarvalhoprado@gmail.com>
2023-05-17 23:29:52 -0300

    Revert "Adicoes DB.json e mais modificacoes"
    This reverts commit 6088035e2979d3e3a04e307f2b4aed189c34fba4.

--- src/pages/Adote/index.js
+++ src/pages/Adote/index.js
@@ -1,21 +1,7 @@
 import Titulo from 'components/Titulo';
 import styles from './Adote.module.css';
-import { useEffect, useState } from 'react';
-import CardUltimasAdocoesInicio from 'components/ComponentsPgInicio/CardsUltimasAdocoesInicio';
 
 function Adote(){
-    const [ultimasAdocoes, setultimasAdocoes] = useState([]);
-
-    useEffect(() => {
-        fetch('https://my-json-server.typicode.com/Andr4Carvalho/AdotaPet/ultimasAdocoes')
-        .then(resposta => resposta.json())
-        .then(dados => {
-            setultimasAdocoes(dados)
-        })
-
-        
-    }, [])
-
     return(
         <div className={styles.linha}>
             <div className={styles.divSidebar}>
@@ -27,11 +13,6 @@ function Adote(){
                 <Titulo>
                     <h2 className={styles.titulo}>Animais disponíveis</h2>
                 </Titulo>
-                <section className={styles.cardsDisponiveis}>
-                    {ultimasAdocoes.map((adocao) => {
-                        return <CardUltimasAdocoesInicio {...adocao} key={adocao.nome} />
-                    })}
-                </section>
             </div>
         </div>
     )
